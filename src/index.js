72863f26652ccba182c2c7fe4b073f1e60b26ff8
Lingabathula Sharath <sharathlingabathula@gmail.com>
2023-01-04 16:28:16 +0530

    few changes were made in .env file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 
 const express = require('express');
-require('dotenv').config();
+
+//importing the PORT from the serverConfig.js file
+const { PORT } = require('./config/serverConfig');
 const setUpAndStartServer = async() =>{
 
    //create an object for express 
    const app  = express();
-   const PORT = 3000;
+  
    app.listen(PORT,() =>{
       console.log(`Server started at ${PORT}`);
-      console.log(process.env);
+      
    });
 }
 setUpAndStartServer();
