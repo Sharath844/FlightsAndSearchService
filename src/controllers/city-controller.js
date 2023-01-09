0c1535196bd1a05497f7e97fc7e49fda9089be47
Aditya Mukherjee <mukherjee.aditya.official@gmail.com>
2023-01-09 20:09:03 +0530

    Made changes to resolve error

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,5 +1,4 @@
-const CityRepository = require('../repository/city-repository');
-const {CityService } = require('../services/index');
+const { CityService } = require('../services/index');
 
 const cityService = new CityService();
 
@@ -82,4 +81,10 @@ const update = async(req, res)=>{
             err : error
         });
     }
+}
+
+module.exports = {
+    create,
+    destroy,
+    update
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,6 @@ const CityController = require('../../controllers/city-controller');
 
 const router = express.Router();
 
-router.post('/city',CityController.create);
+router.post('/city', CityController.create);
 
 module.exports = router;
